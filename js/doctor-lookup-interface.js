b123c20ea3c285871da0da5fdbeabd0584eacf59
LydianLights <rane.fields@gmail.com>
2017-12-01 16:37:19 -0800

    Display info in results list

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -15,15 +15,15 @@ class SearchForm {
     BetterDoctorApi.search(searchQuery, SearchForm.showResults, SearchForm.searchError);
   }
   static showResults(foundPractices) {
-    console.log(foundPractices);
     $("#no-results-found").hide();
     $("#search-results-list").hide();
     if (foundPractices.length > 0) {
       let resultHtml = "";
-      foundPractices.forEach(function(practice) {
-        let searchResult = new SearchResult(practice);
-        resultHtml += searchResult.getHtml();
-      });
+      resultHtml += `<div id="search-results-panels" class="panel-group">`
+      for (let i = 0; i < foundPractices.length; i++) {
+        resultHtml += SearchForm.generateSearchResultHtml(foundPractices[i], i);
+      }
+      resultHtml += `</div>`;
       $("#search-results-list").append(resultHtml);
       $("#search-results-list").show();
     }
@@ -35,20 +35,33 @@ class SearchForm {
     $("#search-form button[type=submit]").removeAttr("disabled");
     $("#search-form-query").val("");
   }
+  static generateSearchResultHtml(practice, resultIndex) {
+    let html = `
+    <div class="search-result panel">
+      <a data-toggle="collapse" data-parent="#search-results-panels" href="#search-result-${resultIndex}">
+        <div class="panel-heading">
+          <h3 class="panel-title">${practice.name} - ${practice.address.city}, ${practice.address.state}</h3>
+        </div>
+      </a>
+      <div id="search-result-${resultIndex}" class="panel-collapse collapse">
+        <div class="panel-body">
+          <p>${practice.doctor.name.fullWithTitle}</p>
+          <p>${practice.address.street}</p>
+          <p>${practice.address.fullCity}</p>
+          <p>${practice.phoneNumber.standardFormat}</p>
+          <hr>
+          <p>Accepting Patients: ${practice.acceptingPatients ? "Yes" : "No"}</p>
+          <p>${practice.doctor.bio}</p>
+        </div>
+      </div>
+    </div>
+    `;
+    return html;
+  }
   static searchError() {
     console.log("Search Error");
     $("#searching-message").hide();
     $("#results").show();
-  }
-}
-
-class SearchResult {
-  constructor(practice) {
-    this.practice = practice;
-  }
-  getHtml() {
-    let html = "<h2>result</h2>";
-    return html;
   }
 }
 
